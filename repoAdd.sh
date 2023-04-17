1151b8938ece4965699926a0e47c57ffff6b4f61
Spark Wan <yshelove@live.com>
2023-04-17 16:43:56 +0800

    update

--- repoAdd.sh
+++ repoAdd.sh
@@ -2,12 +2,16 @@
 
 for _dir in "$@"; do
 
-	if [[ ${_dir} ]]; then
-		#echo ${_dir}
-		pushd /var/db/repos/gentoo/${_dir}
-			sed -i "/src.rpm/d" /var/db/repos/gentoo/${_dir}/Manifest
+	if [[ -d "/var/db/repos/gentoo/${_dir}" ]]; then
+
+		pushd /var/db/repos/gentoo/"${_dir}"
+
+			sed -i "/src.rpm/d" /var/db/repos/gentoo/"${_dir}"/Manifest
+
 			repoman manifest
+
 			x=$?
+
 		        if [[ $x != 0 ]]; then
 		            break
 		        fi
